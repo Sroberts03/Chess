b0846afa979e62286377d5ac12cea786fafa9d6a
Sam ROberts <samr72003@gmail.com>
2025-01-15 14:35:49 -0700

    updates for chess moves

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -4,75 +4,54 @@ import java.util.ArrayList;
 
 public interface PieceMovesCalculator {
 
-    public ArrayList<ChessMove> moves = new ArrayList<>();
+    public static void KingMoveHelper(int endRow, int endCol, ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+        if (((endRow <= 7 && endRow >= 0 ) && (endCol <= 7 && endCol >= 0))
+                && (chessBoard.array[endRow][endCol] == null
+                || chessBoard.array[endRow][endCol].getTeamColor() != teamColor)) {
+            ChessPosition newPosition = new ChessPosition(endRow + 1, endCol + 1);
+            moves.add(new ChessMove(startPosition, newPosition, ChessPiece.PieceType.KING));
+        }
+    }
 
-    public static ArrayList<ChessMove> KingMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> QueenMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
         int startCol = startPosition.getColumn();
         int startRow = startPosition.getRow();
-        //up one row
-        if (chessBoard.array[startRow + 1][startCol] == null && startRow + 1 < chessBoard.array.length) {
-            ChessPosition endPosition = new ChessPosition(startRow + 1, startCol);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //down one row
-        if (chessBoard.array[startRow - 1][startCol] == null && startRow - 1 > 0) {
-            ChessPosition endPosition = new ChessPosition(startRow - 1, startCol);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //one to the right
-        if (chessBoard.array[startRow][startCol + 1] == null && startCol + 1 < 8) {
-            ChessPosition endPosition = new ChessPosition(startRow, startCol + 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //one to the left
-        if (chessBoard.array[startRow][startCol - 1] == null && startCol - 1 > 0) {
-            ChessPosition endPosition = new ChessPosition(startRow, startCol - 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //one up and one to the left
-        if (chessBoard.array[startRow + 1][startCol - 1] == null && startCol - 1 > 0
-                && startRow + 1 < chessBoard.array.length) {
-            ChessPosition endPosition = new ChessPosition(startRow + 1, startCol - 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //todo
-        //one up and one to the right
-        if (chessBoard.array[startRow][startCol - 1] == null && startCol - 1 > 0) {
-            ChessPosition endPosition = new ChessPosition(startRow, startCol - 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //todo
-        //one down and one to the left
-        if (chessBoard.array[startRow][startCol - 1] == null && startCol - 1 > 0) {
-            ChessPosition endPosition = new ChessPosition(startRow, startCol - 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
-        //todo
-        //one down and one to the right
-        if (chessBoard.array[startRow][startCol - 1] == null && startCol - 1 > 0) {
-            ChessPosition endPosition = new ChessPosition(startRow, startCol - 1);
-            moves.add(new ChessMove(startPosition, endPosition, ChessPiece.PieceType.KING));
-        }
         return moves;
     }
 
-    public static ArrayList<ChessMove> QueenMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> BishopMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+        int startCol = startPosition.getColumn();
+        int startRow = startPosition.getRow();
         return moves;
     }
 
-    public static ArrayList<ChessMove> BishopMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> KnightMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+        int startCol = startPosition.getColumn();
+        int startRow = startPosition.getRow();
         return moves;
     }
 
-    public static ArrayList<ChessMove> KnightMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> RookMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+        int startCol = startPosition.getColumn();
+        int startRow = startPosition.getRow();
         return moves;
     }
 
-    public static ArrayList<ChessMove> RookMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
-        return moves;
-    }
-
-    public static ArrayList<ChessMove> PawnMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition) {
+    public static ArrayList<ChessMove> PawnMoveCalculator(ChessBoard chessBoard, ChessPosition startPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+        int startCol = startPosition.getColumn();
+        int startRow = startPosition.getRow();
+        if (startRow == 2 &&  teamColor == ChessGame.TeamColor.WHITE) {
+            ChessMove move_one = new ChessMove(startPosition, new ChessPosition(startRow + 1, startCol), ChessPiece.PieceType.PAWN);
+            ChessMove move_two = new ChessMove(startPosition, new ChessPosition(startRow + 2, startCol), ChessPiece.PieceType.PAWN);
+            moves.add(move_one);
+            moves.add(move_two);
+        }
+        else if (startRow == 7 &&  teamColor == ChessGame.TeamColor.BLACK) {
+            ChessMove move_one = new ChessMove(startPosition, new ChessPosition(startRow - 1, startCol), ChessPiece.PieceType.PAWN);
+            ChessMove move_two = new ChessMove(startPosition, new ChessPosition(startRow - 2, startCol), ChessPiece.PieceType.PAWN);
+            moves.add(move_one);
+            moves.add(move_two);
+        }
         return moves;
     }
 }
