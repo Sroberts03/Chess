52d144780c9f9c04cc2c8ecb7ddd2e7759c73ad0
Sam ROberts <samr72003@gmail.com>
2025-01-15 18:10:46 -0700

    Bishop moves working

---
+++ shared/src/main/java/chess/MovesCalculator/BishopMoveCalculator.java
@@ -0,0 +1,19 @@
+package chess.MovesCalculator;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+
+public class BishopMoveCalculator {
+
+    public static void Bishop_Move_Calculator(ChessBoard board, ChessPosition myPosition,
+                                            ArrayList<ChessMove> moves, ChessGame.TeamColor teamColor) {
+        int startCol = myPosition.getColumn() - 1;
+        int startRow = myPosition.getRow() - 1;
+        BishopAndQueenHelper.Bishop_And_Queen(startRow, startCol,
+                board, myPosition, teamColor, moves);
+    }
+}
