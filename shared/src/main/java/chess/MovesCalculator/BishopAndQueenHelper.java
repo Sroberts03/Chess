370c8a1f735b2e06346ab6b805fe018da467e086
Sam ROberts <samr72003@gmail.com>
2025-01-18 00:27:21 -0700

    edits for quality check

--- shared/src/main/java/chess/MovesCalculator/BishopAndQueenHelper.java
+++ shared/src/main/java/chess/MovesCalculator/BishopAndQueenHelper.java
@@ -9,7 +9,9 @@ import java.util.ArrayList;
 
 public class BishopAndQueenHelper {
 
-    public static void Bishop_And_Queen(int startRow, int startCol, ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor teamColor, ArrayList<ChessMove> moves) {
+    public static void Bishop_And_Queen(int startRow, int startCol, ChessBoard board,
+                                        ChessPosition myPosition, ChessGame.TeamColor teamColor,
+                                        ArrayList<ChessMove> moves) {
         //up and down movement
         for (int i = 1; i < 8; i++) {
             String returned = MoveCalcHelper.Move_Calc_Helper(startRow + i, startCol + i, board, myPosition, teamColor, moves);
