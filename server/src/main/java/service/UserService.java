070d042fd62af7d6e668d14bc29660fc68415d9b
Sam ROberts <samr72003@gmail.com>
2025-02-13 14:50:34 -0700

    handler files

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,18 +1,33 @@
 package service;
 
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
 import resultrequest.*;
+import java.util.Objects;
+import static dataaccess.MemoryAuthDAO.createAuth;
+import static dataaccess.MemoryUserDAO.generateToken;
+import static dataaccess.MemoryUserDAO.getUser;
 
 public class UserService {
 
-    public RegisterResult register(RegisterRequest registerRequest) {
+    public static RegisterResult register(RegisterRequest registerRequest) {
         return null;
     }
 
-    public LoginResult login(LoginRequest loginRequest) {
-        return null;
+    public static LoginResult login(LoginRequest loginRequest) throws DataAccessException {
+        String username = loginRequest.username();
+        String password = loginRequest.password();
+        UserData user = getUser(username);
+        if (!Objects.equals(user.password(), password)) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        AuthData auth = new AuthData(generateToken(),username);
+        createAuth(auth);
+        return new LoginResult(username, auth.authToken());
     }
 
-    public void logout(LogoutRequest logoutRequest) {
+    public static void logout(LogoutRequest logoutRequest) {
 
     }
 
