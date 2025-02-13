f14f369965adbd8c3400e8342f4a00385d2677b5
Sam ROberts <samr72003@gmail.com>
2025-02-13 10:19:22 -0700

    implemented gameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,5 +1,33 @@
 package dataaccess;
 
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryGameDAO implements GameDAO {
 
+    public Map<Integer, GameData> gameMap;
+
+    public MemoryGameDAO() {
+        gameMap = new HashMap<>();
+    }
+
+    public void createGame(GameData game) {
+        Integer gameID = game.gameID();
+        gameMap.put(gameID, game);
+    }
+
+    public GameData getGame(Integer gameID) {
+        return gameMap.get(gameID);
+    }
+
+    public ArrayList<GameData> listGames() {
+        return new ArrayList<>(gameMap.values());
+    }
+
+    public void updateGame(GameData game) {
+        gameMap.put(game.gameID(), game);
+    }
 }
