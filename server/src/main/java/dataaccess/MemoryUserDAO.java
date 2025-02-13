fd616d066fdf456d7c1a37b8d1934b8732655bfa
Sam ROberts <samr72003@gmail.com>
2025-02-13 10:25:43 -0700

    implemented userDAO

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,25 @@
 package dataaccess;
 
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryUserDAO implements UserDAO {
+
+    public Map<String, UserData> userMap;
+
+    public MemoryUserDAO () {
+        userMap = new HashMap<>();
+    }
+
+    public void createUser(UserData user) {
+        String username = user.username();
+        userMap.put(username, user);
+    }
+
+    public UserData getUser(String username) {
+        return userMap.get(username);
+    }
 
 }
