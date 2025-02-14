cb0ab4c55a725005aef5be6912cb2da9e95196c8
Sam ROberts <samr72003@gmail.com>
2025-02-14 11:34:54 -0700

    minor changes to user service and tests

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,43 @@
+package service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GameServiceTests {
+
+    @Test
+    @DisplayName("getGames positive")
+    public void getGamesTest() {
+
+    }
+
+    @Test
+    @DisplayName("getGames negative")
+    public void getGamesTestNeg() {
+
+    }
+
+    @Test
+    @DisplayName("createGame positive")
+    public void createGameTest() {
+
+    }
+
+    @Test
+    @DisplayName("createGame Negative")
+    public void createGameTestNegative() {
+
+    }
+
+    @Test
+    @DisplayName("joinGame positive")
+    public void joinGameTest() {
+
+    }
+
+    @Test
+    @DisplayName("joinGame negative")
+    public void joinGameTestNegative() {
+
+    }
+}
