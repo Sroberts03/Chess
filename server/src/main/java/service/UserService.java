1c10baf1e68c65282b9e79cab38abbed10bb4c0f
Sam ROberts <samr72003@gmail.com>
2025-02-14 11:23:51 -0700

    user service JUNIT tests as well as functional services

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,22 +4,39 @@ import dataaccess.DataAccessException;
 import model.AuthData;
 import model.UserData;
 import resultrequest.*;
-import java.util.Objects;
-import static dataaccess.MemoryAuthDAO.createAuth;
-import static dataaccess.MemoryUserDAO.generateToken;
+
+import static dataaccess.MemoryAuthDAO.*;
+import static dataaccess.MemoryUserDAO.createUser;
 import static dataaccess.MemoryUserDAO.getUser;
 
 public class UserService {
 
-    public static RegisterResult register(RegisterRequest registerRequest) {
-        return null;
+    public static RegisterResult register(RegisterRequest registerRequest) throws DataAccessException {
+        String username = registerRequest.username();
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+        UserData user = getUser(username);
+        AuthData newAuth = null;
+        if (user == null) {
+            UserData newUser = new UserData(username, password, email);
+            createUser(newUser);
+            newAuth = new AuthData(generateToken(), username);
+            createAuth(newAuth);
+        }
+        if (user != null) {
+            throw new DataAccessException("Error: already taken");
+        }
+        return new RegisterResult(username, newAuth.authToken());
     }
 
     public static LoginResult login(LoginRequest loginRequest) throws DataAccessException {
         String username = loginRequest.username();
         String password = loginRequest.password();
         UserData user = getUser(username);
-        if (!Objects.equals(user.password(), password)) {
+        if (user == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        if (!user.password().equals(password)) {
             throw new DataAccessException("Error: unauthorized");
         }
         AuthData auth = new AuthData(generateToken(),username);
@@ -27,8 +44,13 @@ public class UserService {
         return new LoginResult(username, auth.authToken());
     }
 
-    public static void logout(LogoutRequest logoutRequest) {
-
+    public static void logout(LogoutRequest logoutRequest) throws DataAccessException {
+        String auth = logoutRequest.authToken();
+        String otherAuth = getAuth(auth);
+        if (otherAuth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+        removeAuth(auth);
     }
 
 }
