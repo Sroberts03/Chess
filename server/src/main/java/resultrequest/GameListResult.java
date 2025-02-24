f88d9ae4bdf601918813b85ca3eabe44bc680c45
Sam ROberts <samr72003@gmail.com>
2025-02-24 15:34:48 -0700

    list games working

--- server/src/main/java/resultrequest/GameListResult.java
+++ server/src/main/java/resultrequest/GameListResult.java
@@ -3,5 +3,5 @@ package resultrequest;
 import model.GameData;
 import java.util.ArrayList;
 
-public record GameListResult(ArrayList<GameData> gameList) {
+public record GameListResult(ArrayList<GameData> games) {
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -34,7 +34,7 @@ public class Server {
         Spark.post("/user", new RegisterHandler(authDAO, userDAO)); //register
         Spark.post("/session", new LoginHandler(authDAO, userDAO));//login
         Spark.delete("/session", new LogoutHandler(authDAO, userDAO)); //logout
-        Spark.get("/game", new ListGamesHandler()); //listGames
+        Spark.get("/game", new ListGamesHandler(gameDAO, authDAO)); //listGames
         Spark.post("/game", new CreateGameHandler()); //createGame
         Spark.put("/game", new JoinGameHandler()); //joinGame
     }
