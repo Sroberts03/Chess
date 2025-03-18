e941378ba7d1c95249efd52b50ce88371a550495
Sam ROberts <samr72003@gmail.com>
2025-03-18 06:52:27 -0600

    Repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,39 @@
 package ui;
 
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class Repl {
+    private final ui.ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ui.ChessClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println(" Welcome to Chess. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
+
 }
