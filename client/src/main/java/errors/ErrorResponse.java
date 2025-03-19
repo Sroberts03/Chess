dccb42c0f687a43186bf414c4aa2ac455d665e8e
Sam ROberts <samr72003@gmail.com>
2025-03-18 20:30:07 -0600

    Register server facade working

---
+++ client/src/main/java/errors/ErrorResponse.java
@@ -0,0 +1,4 @@
+package errors;
+
+public record ErrorResponse(String message) {
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,66 @@
 package client;
 
+import errors.ResponseException;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String facadeUrl =  "http://localhost:" + Integer.toString(port);
+        facade = new ServerFacade(facadeUrl);
     }
 
     @AfterAll
-    static void stopServer() {
+    static void stopServerAndClearApp() {
+        boolean thrown = false;
+        try {
+            facade.clearApp();
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assert !thrown;
         server.stop();
     }
 
+    @Test
+    @DisplayName("Register positive")
+    public void registerGood() throws ResponseException {
+        boolean thrown = false;
+        AuthData auth = null;
+        UserData user = new UserData("test", "testing321", "test@test.test");
+        try {
+            auth = facade.register(user);
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assert !thrown;
+        assert auth != null;
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("Register negative")
+    public void registerBad() throws ResponseException {
+        boolean thrown = false;
+        AuthData auth = null;
+        UserData user = new UserData("", "testing321", "test@test.test");
+        try {
+            auth = facade.register(user);
+        } catch (ResponseException e) {
+            thrown = true;
+        }
+        assert thrown;
     }
 
 }
